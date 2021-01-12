07bd3b761768664d61db081c2bc969fb6b190714
benfoxall <benfoxall@gmail.com>
2021-01-12 22:01:16 +0000

    Deploying to gh-pages from  @ 74534f8844c66ca0ca102ab1d198ab36e8ce1eb2 🚀

---
+++ dist/index.js
@@ -0,0 +1,3 @@
+import React from "../web_modules/react.js";
+import {render} from "../web_modules/react-dom.js";
+render(/* @__PURE__ */ React.createElement("h1", null, "Demo 2"), document.getElementById("root"));
